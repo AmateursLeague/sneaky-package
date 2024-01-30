84147922c63840716d7f7cc33bcc7d92982eb5b3
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-01-30 20:37:36 +0530

    initiating

---
+++ matplotlib_visual/models.py
@@ -0,0 +1,10 @@
+import os
+import inspect
+
+def lib(snippet_name):
+    snippet_path = os.path.join(os.path.dirname(__file__), 'code_snippets', f'{snippet_name}.py')
+
+    with open(snippet_path, 'r') as file:
+        source_code = file.read()
+
+    print(source_code)
