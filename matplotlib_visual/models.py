dec713181b209db86dec1d0bcd0f334ea0ff76ed
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-02-04 13:20:59 +0530

    package minor-fix 0.2

--- matplotlib_visual/models.py
+++ matplotlib_visual/models.py
@@ -1,7 +1,7 @@
 import os
 import inspect
 
-def lib(snippet_name):
+def display(snippet_name):
     snippet_path = os.path.join(os.path.dirname(__file__), 'code_snippets', f'{snippet_name}.py')
 
     with open(snippet_path, 'r') as file:
