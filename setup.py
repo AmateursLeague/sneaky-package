cfe8da708ff7f4adf30bdad8eb5bbb26a39f7f0f
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-02-12 18:12:11 +0530

    update v0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='matplotlib_visual',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     description='A package for visualizing data using Matplotlib',
     author='mcafin44',
