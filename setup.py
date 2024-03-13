ea4b74b3615ef5a95ef17506d45e29b02367b09d
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-03-13 21:35:16 +0530

    update v0.6

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='matplotlib_visual',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     description='A package for visualizing data using Matplotlib',
     author='mcafin44',
-    author_email='johnmecafin44s@gmail.com',
+    author_email='johnmecafin44@gmail.com',
     license='GPL 3.0',
     install_requires=[
     ],
