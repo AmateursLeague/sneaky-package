d00b8121640a5da0e83cb2659b064f278b3425bf
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-04-07 17:29:44 +0530

    update v0.8

--- matplotlib_visual/graph.py
+++ matplotlib_visual/graph.py
@@ -1,6 +1,16 @@
 import os
-import inspect
-import ctypes
+
+# Operating system detection
+if os.name == 'nt':  # Windows
+    def copy_to_clipboard(text):
+        command = 'echo ' + text.strip() + '| clip'
+        os.system(command)
+elif os.name == 'posix':  # Linux or macOS
+    def copy_to_clipboard(text):
+        command = 'echo ' + text.strip() + '| xclip -selection clipboard'
+        os.system(command)
+else:
+    raise OSError("Unsupported operating system")
 
 def display(snippet_name):
     snippet_path = os.path.join(os.path.dirname(__file__), 'code_snippets', f'{snippet_name}.py')
@@ -8,40 +18,4 @@ def display(snippet_name):
     with open(snippet_path, 'r') as file:
         source_code = file.read()
 
-    # Attempt to copy the source code to the clipboard
-    try:
-        copy_to_clipboard(source_code)
-        print("Source code copied to clipboard.")
-    except Exception as e:
-        print("Error copying to clipboard:", e)
-
-def copy_to_clipboard(text):
-    user32 = ctypes.WinDLL('user32')
-    kernel32 = ctypes.WinDLL('kernel32')
-
-    GMEM_MOVEABLE = 0x0002
-    CF_UNICODETEXT = 13
-
-    kernel32.GlobalAlloc.restype = ctypes.wintypes.HGLOBAL
-    kernel32.GlobalLock.argtypes = [ctypes.wintypes.HGLOBAL]
-    kernel32.GlobalUnlock.argtypes = [ctypes.wintypes.HGLOBAL]
-    kernel32.GlobalSize.argtypes = [ctypes.wintypes.HGLOBAL]
-    user32.SetClipboardData.argtypes = [ctypes.wintypes.UINT, ctypes.wintypes.HANDLE]
-
-    data = text.encode('utf-16le')
-    size = len(data)
-
-    handle = kernel32.GlobalAlloc(GMEM_MOVEABLE, size)
-    if handle:
-        ptr = kernel32.GlobalLock(handle)
-        ctypes.memmove(ptr, ctypes.c_char_p(data), size)
-        kernel32.GlobalUnlock(handle)
-        user32.OpenClipboard(0)
-        user32.EmptyClipboard()
-        user32.SetClipboardData(CF_UNICODETEXT, handle)
-        user32.CloseClipboard()
-    else:
-        raise ctypes.WinError()
-
-# Example usage:
-# display('your_snippet_name')
+    copy_to_clipboard(source_code)
