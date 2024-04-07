85deeafac9b0770547b37fab3e62dcc15dec9167
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-04-07 17:44:28 +0530

    update v0.8.2

--- matplotlib_visual/graph.py
+++ matplotlib_visual/graph.py
@@ -1,16 +1,6 @@
 import os
-
-# Operating system detection
-if os.name == 'nt':  # Windows
-    def copy_to_clipboard(text):
-        command = 'echo ' + text.strip() + '| clip'
-        os.system(command)
-elif os.name == 'posix':  # Linux or macOS
-    def copy_to_clipboard(text):
-        command = 'echo ' + text.strip() + '| xclip -selection clipboard'
-        os.system(command)
-else:
-    raise OSError("Unsupported operating system")
+import subprocess
+import sys
 
 def display(snippet_name):
     snippet_path = os.path.join(os.path.dirname(__file__), 'code_snippets', f'{snippet_name}.py')
@@ -18,4 +8,16 @@ def display(snippet_name):
     with open(snippet_path, 'r') as file:
         source_code = file.read()
 
+    # Function to copy text to clipboard based on platform
+    def copy_to_clipboard(text):
+        # Linux
+        if 'linux' in sys.platform:
+            subprocess.run(['xclip', '-selection', 'clipboard'], input=text.strip().encode(), check=True)
+        # Windows
+        elif 'win32' in sys.platform:
+            subprocess.run('clip', input=text.strip().encode(), check=True)
+        else:
+            raise OSError("Unsupported operating system")
+
+    # Copy source code to clipboard
     copy_to_clipboard(source_code)
