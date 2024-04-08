61a2932805b812eca3a40758b66b5a94a5fb375b
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-04-08 19:28:37 +0530

    update v0.9.2

---
+++ matplotlib_visual/piechart.py
@@ -0,0 +1,9 @@
+import os
+
+def plot(snippet_name):
+    snippet_path = os.path.join(os.path.dirname(__file__), 'code_snippets', f'{snippet_name}.py')
+    output_path = os.path.join(os.path.dirname(__file__), f'{snippet_name}.py')
+    with open(snippet_path, 'r') as file:
+        source_code = file.read()
+    with open(output_path, 'w') as output_file:
+        output_file.write(source_code)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='matplotlib_visual',
-    version='0.9.1',
+    version='0.9.2',
     packages=find_packages(),
     description='A package for visualizing data using Matplotlib',
     author='mcafin44',
