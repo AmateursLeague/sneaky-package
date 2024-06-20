19e06ba06a45f9f2b33e7eb5838baf0603c9c8ef
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-06-20 21:34:52 +0530

    reduce longer line codes with black linter

--- matplotlib_visual/models.py
+++ matplotlib_visual/models.py
@@ -1,10 +1,13 @@
 import os
 import inspect
 
-def display(snippet_name):
-    snippet_path = os.path.join(os.path.dirname(__file__), 'code_snippets', f'{snippet_name}.py')
 
-    with open(snippet_path, 'r') as file:
+def display(snippet_name):
+    snippet_path = os.path.join(
+        os.path.dirname(__file__), "code_snippets", f"{snippet_name}.py"
+    )
+
+    with open(snippet_path, "r") as file:
         source_code = file.read()
 
     print(source_code)
