38fbab78e627f83c30cda1f03e7b1f4bccb230b6
Anuj Saha <153378181+AnujSaha0111@users.noreply.github.com>
2024-10-02 16:48:12 +0530

    Update graph.py

--- package/graph.py
+++ package/graph.py
@@ -5,25 +5,29 @@ import sys
 
 def display(snippet_name):
     snippet_path = os.path.join(
-        os.path.dirname(__file__), "code_snippets", f"{snippet_name}.py"
+        os.path.dirname(__file__), "stash", f"{snippet_name}.py"
     )
 
     with open(snippet_path, "r") as file:
         source_code = file.read()
 
+    # Function to copy text to clipboard based on platform
     def copy_to_clipboard(text):
+        # linux
         if "linux" in sys.platform:
             subprocess.run(
                 ["/usr/bin/xclip", "-selection", "clipboard"],
                 input=text.strip().encode(),
                 check=True,
             )
+        # windows
         elif "win32" in sys.platform:
             subprocess.run(
                 ["C:\\Windows\\System32\\clip.exe"],
                 input=text.strip().encode(),
                 check=True,
             )
+        # macOS
         elif "darwin" in sys.platform:
             subprocess.run(
                 ["/usr/bin/pbcopy"],  # Full path to pbcopy for macOS
