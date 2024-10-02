f513a7603e504e94798df2619b2396b0b9242b8f
Anuj Saha <153378181+AnujSaha0111@users.noreply.github.com>
2024-10-02 15:18:33 +0530

    Update graph.py

--- package/graph.py
+++ package/graph.py
@@ -5,29 +5,30 @@ import sys
 
 def display(snippet_name):
     snippet_path = os.path.join(
-        os.path.dirname(__file__), "stash", f"{snippet_name}.py"
+        os.path.dirname(__file__), "code_snippets", f"{snippet_name}.py"
     )
 
     with open(snippet_path, "r") as file:
         source_code = file.read()
 
-    # Function to copy text to clipboard based on platform
     def copy_to_clipboard(text):
-        # Linux
         if "linux" in sys.platform:
             subprocess.run(
                 ["/usr/bin/xclip", "-selection", "clipboard"],
                 input=text.strip().encode(),
                 check=True,
             )
-
-        # Windows
         elif "win32" in sys.platform:
             subprocess.run(
                 ["C:\\Windows\\System32\\clip.exe"],
                 input=text.strip().encode(),
                 check=True,
             )
-
+        elif "darwin" in sys.platform:
+            subprocess.run(
+                ["/usr/bin/pbcopy"],  # Full path to pbcopy for macOS
+                input=text.strip().encode(),
+                check=True,
+            )
         else:
             raise OSError("Unsupported operating system")
