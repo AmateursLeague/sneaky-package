d0c51171ffffdedf0814d55783f216986e3eba8b
Anuj Saha <153378181+AnujSaha0111@users.noreply.github.com>
2024-10-02 14:57:43 +0530

    Update graph.py

--- matplotlib_visual/graph.py
+++ matplotlib_visual/graph.py
@@ -2,18 +2,8 @@ import os
 import subprocess
 import sys
 
+
 def display(snippet_name):
-    """
-    Displays the contents of a Python code snippet file and copies the source code 
-    to the clipboard based on the operating system.
-
-    Parameters:
-    snippet_name (str): The name of the code snippet file (without .py extension) 
-                        to display and copy.
-
-    Raises:
-    OSError: If the operating system is unsupported for copying to the clipboard.
-    """
     snippet_path = os.path.join(
         os.path.dirname(__file__), "code_snippets", f"{snippet_name}.py"
     )
@@ -22,15 +12,6 @@ def display(snippet_name):
         source_code = file.read()
 
     def copy_to_clipboard(text):
-        """
-        Copies the provided text to the clipboard based on the operating system.
-
-        Parameters:
-        text (str): The text to copy to the clipboard.
-
-        Raises:
-        OSError: If the operating system is unsupported for copying to the clipboard.
-        """
         if "linux" in sys.platform:
             subprocess.run(
                 ["/usr/bin/xclip", "-selection", "clipboard"],
@@ -51,6 +32,3 @@ def display(snippet_name):
             )
         else:
             raise OSError("Unsupported operating system")
-
-    print(source_code)
-    copy_to_clipboard(source_code)
