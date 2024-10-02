28ba774b4fdd0fdd29a1e41d5ccf90a64f7e6f71
Anuj Saha <153378181+AnujSaha0111@users.noreply.github.com>
2024-10-02 11:27:25 +0530

    Update graph.py

--- matplotlib_visual/graph.py
+++ matplotlib_visual/graph.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 import sys
 
-
 def display(snippet_name):
     snippet_path = os.path.join(
         os.path.dirname(__file__), "code_snippets", f"{snippet_name}.py"
@@ -11,23 +10,30 @@ def display(snippet_name):
     with open(snippet_path, "r") as file:
         source_code = file.read()
 
-    # Function to copy text to clipboard based on platform
     def copy_to_clipboard(text):
-        # Linux
+        # linux
         if "linux" in sys.platform:
             subprocess.run(
                 ["/usr/bin/xclip", "-selection", "clipboard"],
                 input=text.strip().encode(),
                 check=True,
             )
-
-        # Windows
+        # windows
         elif "win32" in sys.platform:
             subprocess.run(
                 ["C:\\Windows\\System32\\clip.exe"],
                 input=text.strip().encode(),
                 check=True,
             )
-
+        # macOS
+        elif "darwin" in sys.platform:
+            subprocess.run(
+                ["/usr/bin/pbcopy"],
+                input=text.strip().encode(),
+                check=True,
+            )
         else:
             raise OSError("Unsupported operating system")
+
+    print(source_code)
+    copy_to_clipboard(source_code)
