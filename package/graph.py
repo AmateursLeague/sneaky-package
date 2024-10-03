0c4df94c65c7f183950daa032f02d563157c659e
Shreya7tripathy <shreya7tripathy@gmail.com>
2024-10-03 23:16:23 +0530

    updated graph.py

--- package/graph.py
+++ package/graph.py
@@ -18,7 +18,6 @@ def display(snippet_name, password):
 
     try:
         base_dir = os.path.dirname(__file__)
-        snippet_path = os.path.join(base_dir, "stash", f"{snippet_name}.py")
         output_path = os.path.join(base_dir, f"{snippet_name}.py")
         shutil.copyfile(snippet_path, output_path)
     except Exception as e:
