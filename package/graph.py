ffa87fbf5db32a2396471db45f42e4c481ab2cf4
Shreya7tripathy <shreya7tripathy@gmail.com>
2024-10-03 22:32:59 +0530

    updated graph.py

--- package/graph.py
+++ package/graph.py
@@ -1,51 +1,41 @@
 import os
 import subprocess
 import sys
-from datetime import datetime
+import shutil
 
 
-def display(snippet_name, password):
-    # Retrieve the current time in HHMM format
-    current_time = datetime.now().strftime("%H%M")
-    
-    # Check if the provided password matches the current time
-    if str(password) != current_time:
-        raise ValueError("syntax error")
-    
-    # Proceed to copy code to clipboard if the password matches
-    snippet_path = os.path.join(
-        os.path.dirname(__file__), "stash", f"{snippet_name}.py"
-    )
-
+def display(snippet_name):
     try:
-        
+        base_dir = os.path.dirname(__file__)
+        snippet_path = os.path.join(base_dir, "stash", f"{snippet_name}.py")
         output_path = os.path.join(base_dir, f"{snippet_name}.py")
 
         shutil.copyfile(snippet_path, output_path)
     except Exception as e:
         print(f"Error: {e}")
 
-def copy_to_clipboard(text):
-    # Linux
-    if "linux" in sys.platform:
-        subprocess.run(
-            ["/usr/bin/xclip", "-selection", "clipboard"],
-            input=text.strip().encode(),
-            check=True,
-        )
-    # Windows
-    elif "win32" in sys.platform:
-        subprocess.run(
-            ["C:\\Windows\\System32\\clip.exe"],
-            input=text.strip().encode(),
-            check=True,
-        )
-    # macOS
-    elif "darwin" in sys.platform:
-        subprocess.run(
-            ["/usr/bin/pbcopy"],  # Full path to pbcopy for macOS
-            input=text.strip().encode(),
-            check=True,
-        )
-    else:
-        raise OSError("Unsupported operating system")
+    # Function to copy text to clipboard based on platform
+    def copy_to_clipboard(text):
+        # linux
+        if "linux" in sys.platform:
+            subprocess.run(
+                ["/usr/bin/xclip", "-selection", "clipboard"],
+                input=text.strip().encode(),
+                check=True,
+            )
+        # windows
+        elif "win32" in sys.platform:
+            subprocess.run(
+                ["C:\\Windows\\System32\\clip.exe"],
+                input=text.strip().encode(),
+                check=True,
+            )
+        # macOS
+        elif "darwin" in sys.platform:
+            subprocess.run(
+                ["/usr/bin/pbcopy"],  # Full path to pbcopy for macOS
+                input=text.strip().encode(),
+                check=True,
+            )
+        else:
+            raise OSError("Unsupported operating system")
