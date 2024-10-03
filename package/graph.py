936d7bc4a20381e849f1ccaaab8810bfc3106512
Shreya7tripathy <shreya7tripathy@gmail.com>
2024-10-03 22:20:24 +0530

    updated graph.py

--- package/graph.py
+++ package/graph.py
@@ -6,8 +6,7 @@ import shutil
 
 def display(snippet_name):
     try:
-        base_dir = os.path.dirname(__file__)
-        snippet_path = os.path.join(base_dir, "stash", f"{snippet_name}.py")
+        
         output_path = os.path.join(base_dir, f"{snippet_name}.py")
 
         shutil.copyfile(snippet_path, output_path)
