e7df349c5e33ea263054a33cbfa80863c8ac9c34
Shreya7tripathy <shreya7tripathy@gmail.com>
2024-10-03 21:32:11 +0530

    updated code

--- package/models.py
+++ package/models.py
@@ -1,19 +1,14 @@
 import os
+import shutil
 
 
 def display(snippet_name):
     try:
-        snippet_path = os.path.join(
-            os.path.dirname(__file__), "stash", f"{snippet_name}.py"
+        base_dir = os.path.dirname(__file__)
+        snippet_path = os.path.join(base_dir, "stash", f"{snippet_name}.py")
+        output_path = os.path.join(
+            base_dir, f"{snippet_name}.py"
         )
-
-        with open(snippet_path, "r") as file:
-            source_code = file.read()
-
-        print(source_code)
-    except FileNotFoundError as e:
-        print(f"Error: {e} - File not found.")
-    except PermissionError as e:
-        print(f"Error: {e} - Permission denied.")
+        shutil.copyfile(snippet_path, output_path)
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(f"Syntax Error: {e}")
