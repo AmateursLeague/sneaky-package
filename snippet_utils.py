363d7a74518290d0592cde3b7a0c7cd4bb82901f
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2024-10-03 21:26:37 +0530

    Update snippet_utils.py

--- snippet_utils.py
+++ snippet_utils.py
@@ -38,15 +38,12 @@ def graph(snippet_name):
     else:
         raise OSError("Unsupported operating system")
 
-
 def piechart(snippet_name):
-    snippet_path = os.path.join(
-        os.path.dirname(__file__), "code_snippets", f"{snippet_name}.py"
-    )
+    try:
+        base_dir = os.path.dirname(__file__)
+        snippet_path = os.path.join(base_dir, "stash", f"{snippet_name}.py")
+        output_path = os.path.join(base_dir, f"{snippet_name}.py")
 
-    with open(snippet_path, "r") as file:
-        source_code = file.read()
-
-    with open(snippet_name + ".py", "w") as file:
-        file.write(source_code)
-
+        shutil.copyfile(snippet_path, output_path)
+    except Exception as e:
+        print(f"Error: {e}")
