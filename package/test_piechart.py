ad6933b6575d8231d6f0a731f8b7cfd0a092095b
tannuiscoding <142585725+tannuiscoding@users.noreply.github.com>
2024-10-11 12:47:25 +0530

    Test Cases Completed

--- package/test_piechart.py
+++ package/test_piechart.py
@@ -6,7 +6,6 @@ from .piechart import plot
 def test_plot(monkeypatch):
     def mock_copyfile(inputPath, outputPath):
         pass
-
     monkeypatch.setattr(shutil, "copyfile", mock_copyfile)
     snippet_name = "test"
     plot(snippet_name)
