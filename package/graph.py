2c74f34f0fde66364a81e5b6bb9696727506f729
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-10-13 10:16:22 +0530

    fixed graph.py

--- package/graph.py
+++ package/graph.py
@@ -2,50 +2,51 @@ import os
 import subprocess
 import sys
 from datetime import datetime
-import shutil
 
 def display(snippet_name, password):
     # Retrieve the current time in HHMM format
     current_time = datetime.now().strftime("%H%M")
+    
     # Check if the provided password matches the current time
     if str(password) != current_time:
-        raise ValueError("syntax error")
+        raise ValueError("Invalid password")
+    
     # Proceed to copy code to clipboard if the password matches
     snippet_path = os.path.join(
         os.path.dirname(__file__), "stash", f"{snippet_name}.py"
     )
-    try:
-        base_dir = os.path.dirname(__file__)
-        output_path = os.path.join(base_dir, f"{snippet_name}.py")
-        shutil.copyfile(snippet_path, output_path)
-    except Exception as e:
-        print(f"Syntax Error: {e}")
 
-def copy_to_clipboard(text):
+    try:
+        with open(snippet_path, "r") as file:
+            source_code = file.read()
+            
+        # Call the copy to clipboard function
+        copy_to_clipboard(source_code)
+     
+    except FileNotFoundError:
+        print("File not found")
+        raise
+
+def copy_to_clipboard(source_code):
     # Linux
     if "linux" in sys.platform:
-        # Check if xclip is installed
-        if shutil.which("xclip") is None:
-            print("Error: xclip not found. Install it. ", file=sys.stderr)
-            return
-        # If xclip is installed, proceed with copying text
         subprocess.run(
-            ["xclip", "-selection", "clipboard"],
-            input=text.strip().encode(),
+            ["/usr/bin/xclip", "-selection", "clipboard"],
+            input=source_code.strip().encode(),
             check=True,
         )
     # Windows
     elif "win32" in sys.platform:
         subprocess.run(
             ["C:\\Windows\\System32\\clip.exe"],
-            input=text.strip().encode(),
+            input=source_code.strip().encode(),
             check=True,
         )
     # macOS
     elif "darwin" in sys.platform:
         subprocess.run(
             ["/usr/bin/pbcopy"],  # Full path to pbcopy for macOS
-            input=text.strip().encode(),
+            input=source_code.strip().encode(),
             check=True,
         )
     else:
