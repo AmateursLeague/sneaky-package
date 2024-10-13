0e345fd9e4c1603a030051f15aeb549cedf1b331
Tannu Choudhary <142585725+tannuiscoding@users.noreply.github.com>
2024-10-13 10:09:09 +0530

    Fixed the test cases (#112)
    * Fixed the test cases
    
    * fixed the issues suggested in pr

--- tests/test_piechart.py
+++ tests/test_piechart.py
@@ -1,13 +1,60 @@
-import os
-import pytest
 import shutil
+import glob
+from datetime import datetime
 from package.piechart import plot  
 
-def test_plot(monkeypatch):
-    def mock_copyfile(inputPath, outputPath):
-        pass
-    monkeypatch.setattr(shutil, "copyfile", mock_copyfile)
-    snippet_name = "test"
-    plot(snippet_name)
+#we cannot mock this function, so mocking the whole class
+class MockDateTime(datetime):
+    @classmethod
+    def now(cls):
+        return datetime.strptime("1234", "%H%M")
 
-    assert True 
+def mock_copyfile(src, dst):
+    pass  
+
+def mock_glob_single(pattern):
+    return ["stash/test.py"]
+
+def mock_glob_none(pattern):
+    return []
+
+def mock_glob_multiple(pattern):
+    return ["stash/test.py", "stash/test2.py"]
+
+def test_plot_success(monkeypatch, capfd):
+    monkeypatch.setattr("package.piechart.datetime", MockDateTime)
+    monkeypatch.setattr(shutil, "copyfile", mock_copyfile)
+    monkeypatch.setattr(glob, "glob", mock_glob_single)
+    
+    snippet_name = "test"
+    password = "1234"
+    plot(snippet_name, password)
+
+    # Verify output
+    captured = capfd.readouterr()
+    assert "File 'stash/test.py' copied successfully" in captured.out
+
+
+def test_plot_no_file_found(monkeypatch, capfd):
+    monkeypatch.setattr("package.piechart.datetime", MockDateTime)
+    monkeypatch.setattr(glob, "glob", mock_glob_none)
+    
+    snippet_name = "nonexistent"
+    password = "1234"
+    plot(snippet_name, password)
+    
+    # Verify output
+    captured = capfd.readouterr()
+    assert "File is not found" in captured.out
+
+def test_plot_multiple_files_found(monkeypatch, capfd):
+    monkeypatch.setattr("package.piechart.datetime", MockDateTime)
+    monkeypatch.setattr(glob, "glob", mock_glob_multiple)
+    
+    snippet_name = "test"
+    password = "1234"
+    plot(snippet_name, password)
+    
+    # Verify output
+    captured = capfd.readouterr()
+    assert "The given values are not supported" in captured.out
