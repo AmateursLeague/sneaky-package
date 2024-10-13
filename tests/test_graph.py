d116e65892e80da1a22dc756ec835d0c680901e3
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-10-13 10:11:31 +0530

    fixed test_graph.py

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,15 +1,19 @@
 import pytest
-from package.graph import display,copy_to_clipboard
+from package.graph import display
 from datetime import datetime
 import subprocess
+from package.graph import copy_to_clipboard
+import os
+import sys
+import shutil
 
 def test_display_right_password():
     current_time = datetime.now().strftime("%H%M")
-    assert display("test",current_time) is None 
+    assert display("test",current_time) == None 
 
 def test_display_wrong_password():
-    with pytest.raises(ValueError, match="syntax error"):
-        display("test",1234)
+    with pytest.raises(ValueError, match="Invalid password"):
+        display("test", 1111)
 
 def test_copy_to_clipboard(monkeypatch):
     def mock_run(*args, **kwargs):
@@ -19,4 +23,4 @@ def test_copy_to_clipboard(monkeypatch):
     try: 
         copy_to_clipboard("Whatever test")
     except Exception as e:
-        pytest.fail(f"Unexpected error raised: {e}")
+        pytest.fail("Unexpected error raised: {e}")
