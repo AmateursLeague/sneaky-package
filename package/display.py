b5402787a13e7a6176f01250436527e9f1c482d8
tannuiscoding <tannuchoudhary2000@gmail.com>
2024-10-15 22:00:30 +0530

    worked on display

--- package/display.py
+++ package/display.py
@@ -1,14 +1,6 @@
 from package.show import display
 from package.write import plot
 
-def display(method, snippet_name, password, clipboard=None):
-    if method == "show":
-        display(snippet_name, password, clipboard)
-    elif method == "write":
-        write_plot(snippet_name, password)
-    else:
-        raise ValueError(f"Unknown method '{method}' specified. Valid methods are 'clip', 'show', or 'write'.")
-
 def write_plot(snippet_name, password):
     return plot(snippet_name, password)
     
